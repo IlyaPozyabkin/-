f1e9f8bf76382d7f3b6ac0f71cf025c8500e1a40
Ilya <lisss.pod.kaifom@gmail.com>
2022-08-22 20:28:22 +0300

    задача 1

---
+++ Домашка/Задача 1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a < b)
+{
+    Console.WriteLine("Максимальное число: " + b); Console.WriteLine("Минимальное число: " + a);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + a); Console.WriteLine("Минимальное число: " + b);
+}
