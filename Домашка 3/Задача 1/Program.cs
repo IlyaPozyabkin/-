c66b27b140dd309418c6a3ed60b33ec701e9b3f2
Ilya <lisss.pod.kaifom@gmail.com>
2022-08-31 19:33:37 +0300

    задача 1

---
+++ Домашка 3/Задача 1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите пятизначное число: ");
+string? num = Console.ReadLine();
+
+
+void PoliNum(string num)
+{
+    if (num[0] == num[4] || num[1] == num[3])
+    {
+        Console.WriteLine($"Ваше число палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Ваше не число палиндром");
+    }
+
+}
+if (num!.Length == 5)
+{
+   PoliNum(num);
+}
+else if (num!.Length > 5)
+{
+    Console.WriteLine($"Ваше число имеет больше пяти знаков");
+}
+else if ((num!.Length < 5))
+{
+    Console.WriteLine($"Ваше число имеет меньше пяти знаков");
+} 
+
+
