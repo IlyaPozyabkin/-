32c8e5d73bc76d12393e4e2dcc36369f2b604d81
Ilya <lisss.pod.kaifom@gmail.com>
2022-09-05 17:04:12 +0300

    задача 2

---
+++ Домашка 4/Задача 2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int n = i % 10;
+i = i / 10;
+sum = sum + n;
+}
+Console.WriteLine("Сумма цифр в числе равна: " + sum);
