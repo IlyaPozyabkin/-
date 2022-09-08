8616bfb16b497d9275bb9aa3374aa37f540a6e0d
Ilya <lisss.pod.kaifom@gmail.com>
2022-09-08 12:21:17 +0300

    задача 2

---
+++ Домашка 5/Задача 2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.Write("В массиве: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int j = 1; j < numbers.Length; j+=2)
+    sum = sum + numbers[j];
+
+    Console.Write($" всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+}
