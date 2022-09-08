3e5c686dd900ef8df896d06f8645c1e34110bd03
Ilya <lisss.pod.kaifom@gmail.com>
2022-09-08 12:08:43 +0300

    задача 1

--- Домашка 5/Задача 1/Program.cs
+++ Домашка 5/Задача 1/Program.cs
@@ -6,8 +6,8 @@ Console.Write("Массив: ");
 PrintArray(numbers);
 int count = 0;
 
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
+for (int j = 0; j < numbers.Length; j++)
+if (numbers[j] % 2 == 0)
 count++;
 
 Console.Write($" имеет {numbers.Length} чисел, {count} из них чётные");
