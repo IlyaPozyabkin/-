28152a87e3f1939ec1f37b2dcc6e221e164f9525
Ilya <lisss.pod.kaifom@gmail.com>
2022-09-12 18:47:37 +0300

    задача 3

---
+++ Домашка 2/Задача 3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int Number) {
+  if (Number == 6 || Number == 7) {
+  Console.WriteLine($"{Number} день недели является выходным");
+  }
+  else if (Number < 1 || Number > 7) {
+    Console.WriteLine($"Число {Number} не является днём недели");
+  }
+  else Console.WriteLine($"{Number} день недели не является выходным");
+}
+
+CheckingTheDayOfTheWeek(Number);
+
