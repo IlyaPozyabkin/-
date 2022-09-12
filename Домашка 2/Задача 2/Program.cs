438d95712af0d0b20f8255aa6f3f06425cb79aa8
Ilya <lisss.pod.kaifom@gmail.com>
2022-09-12 18:27:53 +0300

    задача 2

---
+++ Домашка 2/Задача 2/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = Read("Введите число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3)
+{
+    Console.WriteLine("Третьей цифры не существует");
+}
+else
+{
+    Console.WriteLine("Третья цифра - " + number.ToString()[2]);
+}
+ 
+int Read(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
